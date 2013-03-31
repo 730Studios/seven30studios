12be24f4a3eada84e5f0dae35e80f891093df9b9
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2013-03-31 01:03:06 -0400

    changed to simpler page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
 		javascript_include_tag("http://use.typekit.com/#{apikey}.js") +
 		javascript_tag("try{Typekit.load()}catch(e){}")
 	end
+	
+	def page_container_id(page_id)
+		default_page_id = "parent-content_container"
+		unless page_id.empty?
+			page_id
+		else
+			default_page_id
+		end
+	end
 end
